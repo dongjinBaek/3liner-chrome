33af1e6996e4e9be54da100cbc352363717a5b7a
dongjinBaek <dj9136@gmail.com>
2022-02-11 13:47:20 +0900

    (TLNR-17) uninstall url 추가

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(function(details) {
+  /* other 'reason's include 'update' */
+  // if (details.reason === "install") {
+      var uninstallUrlLink = 'https://forms.gle/RGDDTWCDQrHUsgnq5';
+      if (chrome.runtime.setUninstallURL) {
+          chrome.runtime.setUninstallURL(uninstallUrlLink);
+      }
+  // }
+});
