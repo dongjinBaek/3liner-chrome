ebefb1514f547a14e96cb23f2f4f35d378178f72
dongjinBaek <dj9136@gmail.com>
2022-02-13 16:30:15 +0900

    set enable preview, anonymousId on start

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
 chrome.runtime.onInstalled.addListener(function(details) {
   /* other 'reason's include 'update' */
   if (details.reason === 'install') {
-      var uninstallUrlLink = 'https://forms.gle/RGDDTWCDQrHUsgnq5';
+      const uninstallUrlLink = 'https://forms.gle/RGDDTWCDQrHUsgnq5';
       if (chrome.runtime.setUninstallURL) {
           chrome.runtime.setUninstallURL(uninstallUrlLink);
       }
+
+      chrome.storage.sync.set({'enablePreview': true}, ()=>{});
+      chrome.storage.sync.set({'anonymousID': generateAnonymousID()}, ()=>{});
   }
 });
+
+const generateAnonymousID = () => {
+  return `AN${(new Date()).getTime().toString(16).toUpperCase()}${Math.floor(Math.random() * 4096).toString(16).toUpperCase()}`;
+}
