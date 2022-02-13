403498555a04a3db3defb48f74415531902a8c41
dongjinBaek <dj9136@gmail.com>
2022-02-13 13:43:10 +0900

    update가 아닌 install 시에만 uninstall url 등록하도록 변경

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.runtime.onInstalled.addListener(function(details) {
   /* other 'reason's include 'update' */
-  // if (details.reason === "install") {
+  if (details.reason === 'install') {
       var uninstallUrlLink = 'https://forms.gle/RGDDTWCDQrHUsgnq5';
       if (chrome.runtime.setUninstallURL) {
           chrome.runtime.setUninstallURL(uninstallUrlLink);
       }
-  // }
+  }
 });
