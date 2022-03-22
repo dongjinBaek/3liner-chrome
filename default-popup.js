65bcafa0e42c0adb74aa1edea864adec512422a2
dongjinBaek <dj9136@gmail.com>
2022-03-22 09:44:57 +0900

    팝업 로깅 추가

--- default-popup.js
+++ default-popup.js
@@ -1,3 +1,5 @@
+amplitude.getInstance().init(AMPLITUDE_KEY);
+
 async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
@@ -7,6 +9,10 @@ async function getCurrentTab() {
 let lines = 3;
 let content = '';
 let sentences = [];
+
+const amplitudeEventProperties = {
+  version: VERSION,
+};
 
 window.onload = async () => {
   const summary = (lines) => {
@@ -25,9 +31,19 @@ window.onload = async () => {
     let shareContent = textRank.summarizedArticle.map((s, idx) => 
       `${idx + 1}. ${s}`
     ).join('3l-new-line');
-    document.getElementById('share-facebook').href = `https://www.facebook.com/sharer/sharer.php?u=${url}&redirect_uri=https://www.facebook.com&3liner-summary=${shareContent}`;
+
+    amplitudeEventProperties.summary = textRank.summarizedArticle;
+    amplitude.getInstance().logEvent('summary loaded', amplitudeEventProperties);
+
+    document.getElementById('share-facebook').addEventListener('click', () => {
+      amplitude.getInstance().logEvent('share summary', {...amplitudeEventProperties, to:'facebook'});
+      chrome.tabs.create({ url: `https://www.facebook.com/sharer/sharer.php?u=${url}&redirect_uri=https://www.facebook.com&3liner-summary=${shareContent}` });
+    });
+    document.getElementById('share-careerly').addEventListener('click', () => {
+      amplitude.getInstance().logEvent('share summary', {...amplitudeEventProperties, to:'careerly'});
+      chrome.tabs.create({ url: `https://careerly.co.kr/posts/create?3liner-summary=${shareContent}&3liner-url=${url}` });
+    });
     // document.getElementById('share-linkedin').href = `https://www.linkedin.com/sharing/share-offsite/?url=${url}?3liner-summary=${shareContent}`;
-    document.getElementById('share-careerly').href = `https://careerly.co.kr/posts/create?3liner-summary=${shareContent}&3liner-url=${url}`;
   }
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -35,6 +51,14 @@ window.onload = async () => {
       const contentElement = document.getElementById('content');
       document.getElementById('document-title').textContent = response.title;
       document.getElementById('document-url').textContent = response.url;
+      amplitudeEventProperties.title = response.title;
+      amplitudeEventProperties.url = response.url;
+      amplitude.getInstance().logEvent('open summary popup', amplitudeEventProperties);
+
+      document.getElementById('feedback-link').addEventListener('click', () => {
+        amplitude.getInstance().logEvent('feedback', amplitudeEventProperties);
+        chrome.tabs.create({ url: `https://forms.gle/VCmjUZD8n75KJPaQ6` });
+      });
 
       // TODO: 더 잘 나누기. 닫는 태그도 textParts에 포함하게 하기.
       const textParts = response.content.split(/(<\/(p|div|h1|h2|h3|h4|h5|h6)>)|(<br\/?>)/gi);
